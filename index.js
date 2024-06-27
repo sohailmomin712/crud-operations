86cf801d3c3ec2fce18c6f3ba05a5dbd1e111d59
sohailmomin712 <sohailmomin053@gmail.com>
2024-06-27 14:58:02 +0530

    appended data dynamically using js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const appenData = (data) => {
   let formTag = document.querySelector("form");
 
   data.forEach((el) => {
-    console.log(el);
+    // console.log(el);
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     td1.innerHTML = el.employeeName;
@@ -22,16 +22,30 @@ const appenData = (data) => {
     td2.innerHTML = el.employeeID;
 
     let td3 = document.createElement("td");
-    td2.innerHTML = el.department;
+    td3.innerHTML = el.department;
 
     let td4 = document.createElement("td");
-    td2.innerHTML = el.experience;
+    td4.innerHTML = el.experience;
 
     let td5 = document.createElement("td");
-    td2.innerHTML = el.email;
+    td5.innerHTML = el.email;
 
     let td6 = document.createElement("td");
-    td2.innerHTML = el.mobile;
+    td6.innerHTML = el.mobile;
+
+    let td7 = document.createElement("td");
+
+    // console.log(td1, td2, td3, td4, td5, td6)
+    tr.append(td1, td2, td3, td4, td5, td6, td7);
+    let tbody = document.querySelector("tbody");
+    tbody.append(tr);
+    if (el.experience < 2) {
+      td7.innerHTML = "Fresher";
+    } else if (el.experience >= 2 && el.experience <= 4) {
+      td7.innerHTML = "Junior";
+    } else {
+      td7.innerHTML = "Senior";
+    }
   });
 };
 
