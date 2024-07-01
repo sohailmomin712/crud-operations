a168e7497c4e00b9d36088194cd53535267aa93b
sohailmomin712 <sohailmomin053@gmail.com>
2024-07-01 19:24:44 +0530

    updated and added edit option

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let isEdit = false;
+
 let formTag = document.querySelector("form");
 
 const getData = async () => {
@@ -28,17 +30,57 @@ const deleteData = async (id) => {
 
 const postData = async (obj) => {
   try {
-    let res = await fetch("http://localhost:3000/employees",{
-      method:"POST",
+    let res = await fetch("http://localhost:3000/employees", {
+      method: "POST",
       headers: {
-        'Content-type': 'application/json; charset=UTF-8' // Indicates the content 
-       },
-       body: JSON.stringify(obj) // We send data in JSON format
+        "Content-type": "application/json; charset=UTF-8", // Indicates the content
+      },
+      body: JSON.stringify(obj), // We send data in JSON format
     });
     let data = await res.json();
-  console.log(data)
+    console.log(data);
   } catch (err) {
     console.log(err);
+  }
+};
+
+const handleEdit = async (data, event, type) => {
+  let btn = event.target;
+  let parentNode = btn.parentNode;
+  let updateBtn = document.querySelector(".submitBtn");
+  let submitBtn = document.querySelector(".submit");
+
+  if(isEdit==true && type == "handle-edit"){
+    return alert("Please close the previous edit button")
+  }
+
+  if (type == "handle-edit") {
+    isEdit = true;
+    btn.style.display = "none";
+
+    parentNode.querySelector(".close-btn").style.display = "block";
+
+    updateBtn.style.display = "block";
+
+    submitBtn.style.display = "none";
+
+    console.log(data);
+    document.getElementById("name").value = data.employeeName;
+    document.getElementById("employeeID").value = data.employeeID;
+    document.getElementById("department").value = data.department;
+    document.getElementById("exp").value = data.experience;
+    document.getElementById("email").value = data.email;
+    document.getElementById("mbl").value = data.mobile;
+  } else {
+    isEdit = false;
+    btn.style.display = "none";
+    parentNode.querySelector(".edit-btn").style.display = "block";
+
+    updateBtn.style.display = "none";
+
+    submitBtn.style.display = "block";
+
+    formTag.reset();
   }
 };
 
@@ -55,11 +97,12 @@ const getFormData = (e) => {
 
   postData(obj);
 };
+
 const appendData = (data) => {
   let tbody = document.querySelector("tbody");
   tbody.innerHTML = ""; // Clear existing data
 
-  data.forEach((el) => {
+  data.forEach((el, index) => {
     let tr = document.createElement("tr");
     tr.setAttribute("id", el.id);
 
@@ -83,18 +126,36 @@ const appendData = (data) => {
 
     let td7 = document.createElement("td");
 
-    let td8 = document.createElement("button");
+    let td8 = document.createElement("td");
     td8.addEventListener("click", (e) => {
       e.preventDefault(); // Prevent default behavior
 
       deleteData(el.id);
     });
     td8.innerHTML = "Delete";
-    td8.style.background = "green";
+    td8.style.background = "red";
     td8.style.color = "white";
-    td8.setAttribute("type", "button"); // Explicitly set type to button
 
-    tr.append(td1, td2, td3, td4, td5, td6, td7, td8);
+    let td9 = document.createElement("td");
+    td9.setAttribute("class", "edit-btn");
+    td9.addEventListener("click", (e) => {
+      handleEdit(el, e, "handle-edit");
+    });
+    td9.innerHTML = "Edit";
+    td9.style.background = "green";
+    td9.style.color = "white";
+
+    let td10 = document.createElement("td");
+    td10.setAttribute("class", "close-btn");
+    td10.addEventListener("click", (e) => {
+      handleEdit(null, e, "handle-close");
+    });
+    td10.innerHTML = "X";
+    td10.style.background = "blue";
+    td10.style.color = "white";
+    td10.style.display = "none";
+
+    tr.append(td1, td2, td3, td4, td5, td6, td7, td8, td9, td10);
 
     tbody.append(tr);
 
